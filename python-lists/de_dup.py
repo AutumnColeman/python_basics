485c0634f9513d1ebf7c209e22f6113883638b8d
Autumn Coleman <autumn.eve.coleman@gmail.com>
2017-01-31 14:09:42 -0500

    Remembering Python

--- python-lists/de_dup.py
+++ python-lists/de_dup.py
@@ -1,11 +1,13 @@
 print "Given an list of numbers or strings, create a new list containing the same elements as the first list, except with any duplicate values removed. Print the list."
 
-# With set function
 exampleList = [-1,0,1,1,2,3,4,5,6,7,7,7,8,8,9,10]
-newList = set(exampleList)
 
-print newList
+# With set function
+# newList = set(exampleList)
+#
+# print newList
 
+# With loop
 newList = []
 
 for i in exampleList:
--- python-lists/matrix_addition.py
+++ python-lists/matrix_addition.py
@@ -1,1 +1,32 @@
-print "Given two two-dimensional lists of numbers of the size 2x2 two dimensional list is represented as an list of lists:"
+print "Given two two-dimensional lists of numbers of the size 2x2 two dimensional list is represented as a list of lists:"
+
+# With numpy
+# import numpy as np
+#
+# Mat1 = np.matrix([[2, -2],
+#           [5, 3]])
+# Mat2 = np.matrix([[5, 2],
+#           [1, 0]])
+#
+# print Mat1 + Mat2
+
+# With empty matrix
+Mat1 = [[2, -2],
+        [5, 3]]
+Mat2 = [[5, 2],
+        [1, 0]]
+sumMat = [[0,0],[0,0]]
+
+for i in range(len(Mat1)):
+    for j in range(len(Mat1[0])):
+        sumMat[i][j] = Mat1[i][j] + Mat2[i][j]
+
+for s in sumMat:
+    print(s)
+
+
+
+sumMat0 = (Mat1[0][0]+Mat2[0][0])
+sumMat1 = (Mat1[0][1]+Mat2[0][1])
+sumMat2 = (Mat1[1][0]+Mat2[1][0])
+sumMat3 = (Mat1[0][0]+Mat2[0][0])
